8867d09e84d9c41d616b05280dfc43dcbcb71953
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-09-14 14:22:05 -0300

    backend refatored to DDD architecture

---
+++ server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { parseISO } from 'date-fns';
+import { container } from 'tsyringe';
+
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+export default class ApointmentsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { provider_id, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = container.resolve(CreateAppointmentService);
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return response.json(appointment);
+  }
+
+  // public async find(request:Request, response:Response): Promise<Response>{
+
+    
+  //   // const appointments = await appointmentsRepository.find();
+
+  //    return response.json('a');
+  // });
+}
