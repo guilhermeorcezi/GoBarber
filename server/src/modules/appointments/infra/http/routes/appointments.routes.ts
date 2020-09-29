d499c445a679726f044ceddfdc0a79129b7a6111
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-09-29 12:39:57 -0300

    routes validation with celebrate

--- server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
@@ -10,8 +11,17 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', appointmentsController.find);
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
+
 appointmentsRouter.get('/me', providerAppointmentsController.create);
 
 export default appointmentsRouter;
--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import cors from 'cors';
 import uploadConfig from '@config/upload';
@@ -18,18 +19,20 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
-// app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-//   if (err instanceof AppError) {
-//     return response.status(err.statusCode).json({
-//       status: 'error',
-//       message: err.message,
-//     });
-//   }
+app.use(errors());
 
-//   return response.status(500).json({
-//     status: 'error',
-//     message: 'Internal server error',
-//   });
-// });
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333);
